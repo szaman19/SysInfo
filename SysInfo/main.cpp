31307236b4c2d509b0b3396e58110dc6edfd2df2
Shehtab Zaman <shehtabzaman@gmail.com>
2016-06-02 19:29:09 -0400

    Initial Commit

---
+++ SysInfo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SysInfo
+//
+//  Created by Shehtab Zaman on 6/2/16.
+//  Copyright © 2016 szaman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
